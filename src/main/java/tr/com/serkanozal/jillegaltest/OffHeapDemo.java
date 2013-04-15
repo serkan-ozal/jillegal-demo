41de04db3ecbfc78d1cb6883cd73c7fa984bc99e
unknown <Asus@Asus-Bilgisayar.(none)>
2013-04-15 09:05:30 +0300

    Update due to update for jillegal

--- src/main/java/tr/com/serkanozal/jillegaltest/OffHeapDemo.java
+++ src/main/java/tr/com/serkanozal/jillegaltest/OffHeapDemo.java
@@ -7,62 +7,243 @@
 
 package tr.com.serkanozal.jillegaltest;
 
-import tr.com.serkanozal.jillegal.offheap.domain.builder.pool.SequentialObjectPoolCreateParameterBuilder;
-import tr.com.serkanozal.jillegal.offheap.domain.model.pool.SequentialObjectPoolCreateParameter.SequentialObjectPoolReferenceType;
-import tr.com.serkanozal.jillegal.offheap.pool.EagerReferencedObjectPool;
-import tr.com.serkanozal.jillegal.offheap.pool.LazyReferencedObjectPool;
-import tr.com.serkanozal.jillegal.offheap.pool.RandomAccessOffHeapPool;
+import java.util.ArrayList;
+import java.util.List;
+
+import tr.com.serkanozal.jillegal.offheap.domain.builder.pool.ArrayOffHeapPoolCreateParameterBuilder;
+import tr.com.serkanozal.jillegal.offheap.domain.builder.pool.DefaultExtendableObjectOffHeapPoolCreateParameterBuilder;
+import tr.com.serkanozal.jillegal.offheap.domain.builder.pool.ExtendableObjectOffHeapPoolCreateParameterBuilder;
+import tr.com.serkanozal.jillegal.offheap.domain.builder.pool.SequentialObjectOffHeapPoolCreateParameterBuilder;
+import tr.com.serkanozal.jillegal.offheap.domain.model.pool.ObjectPoolReferenceType;
+import tr.com.serkanozal.jillegal.offheap.pool.impl.ComplexTypeArrayOffHeapPool;
+import tr.com.serkanozal.jillegal.offheap.pool.impl.EagerReferencedObjectOffHeapPool;
+import tr.com.serkanozal.jillegal.offheap.pool.impl.ExtendableObjectOffHeapPool;
+import tr.com.serkanozal.jillegal.offheap.pool.impl.LazyReferencedObjectOffHeapPool;
+import tr.com.serkanozal.jillegal.offheap.pool.impl.PrimitiveTypeArrayOffHeapPool;
 import tr.com.serkanozal.jillegal.offheap.service.OffHeapService;
 import tr.com.serkanozal.jillegal.offheap.service.OffHeapServiceFactory;
 
 public class OffHeapDemo {
 
-	final static int OBJECT_COUNT = 1000;
+	private static final int ELEMENT_COUNT = 1000;
+	private static final int TOTAL_ELEMENT_COUNT = 10000;
 	
 	public static void main(String[] args) throws Exception {
 		OffHeapService offHeapService = OffHeapServiceFactory.getOffHeapService();
-		
-		
-		
-		LazyReferencedObjectPool<SampleClass> lazyReferencedSequentialObjectPool = 
-				offHeapService.createOffHeapPool(
-						new SequentialObjectPoolCreateParameterBuilder<SampleClass>().
-								type(SampleClass.class).
-								objectCount(OBJECT_COUNT).
-								referenceType(SequentialObjectPoolReferenceType.LAZY_REFERENCED).
-							build()
-				);
-		System.out.println("Lazy Referenced Sequential Off Heap ObjectPool for class " + 
-				SampleClass.class.getName() + " has been allocated ...");
-		runDemoForObjectPool(lazyReferencedSequentialObjectPool);
-		System.out.println("\n\n");
-		
-		
-		EagerReferencedObjectPool<SampleClass> eagerReferencedSequentialObjectPool = 
-				offHeapService.createOffHeapPool(
-						new SequentialObjectPoolCreateParameterBuilder<SampleClass>().
-								type(SampleClass.class).
-								objectCount(OBJECT_COUNT).
-								referenceType(SequentialObjectPoolReferenceType.EAGER_REFERENCED).
-							build()
-				);
-		System.out.println("Eager Referenced Sequential Off Heap ObjectPool for class " + 
-				SampleClass.class.getName() + " has been allocated ...");
-		runDemoForObjectPool(eagerReferencedSequentialObjectPool);
-		System.out.println("\n\n");
+
+		demoLazyReferencedObjectOffHeapPool(offHeapService);
+		demoEagerReferencedObjectOffHeapPool(offHeapService);
+		demoComplexTypeArrayOffHeapPool(offHeapService);
+		demoPrimitiveTypeArrayOffHeapPool(offHeapService);
+		demoExtendableObjectOffHeapPoolWithLazyReferenceObjectOffHeapPool(offHeapService);
+		demoExtendableObjectOffHeapPoolWithEagerReferenceObjectOffHeapPool(offHeapService);
+		demoExtendableObjectOffHeapPoolWithDefaultObjectOffHeapPool(offHeapService);
 	}
 	
-	private static void runDemoForObjectPool(RandomAccessOffHeapPool<SampleClass , ?> offHeapPool) {
-    	for (int i = 0; i < OBJECT_COUNT; i++) {
-    		SampleClass obj = offHeapPool.newObject();
+	private static void demoLazyReferencedObjectOffHeapPool(OffHeapService offHeapService) {
+		LazyReferencedObjectOffHeapPool<SampleClass> lazyReferencedSequentialObjectPool = 
+				offHeapService.createOffHeapPool(
+						new SequentialObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								type(SampleClass.class).
+								objectCount(ELEMENT_COUNT).
+								referenceType(ObjectPoolReferenceType.LAZY_REFERENCED).
+							build());
+							
+		System.out.println("Lazy Referenced Sequential Off Heap Object Pool for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = lazyReferencedSequentialObjectPool.get();
     		obj.setOrder(i);
     		System.out.println("New object has been retrieved from off heap pool and set order value to " + i);
     	}
     	
-    	for (int i = 0; i < OBJECT_COUNT; i++) {
-    		SampleClass obj = offHeapPool.getObject(i);
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = lazyReferencedSequentialObjectPool.getAt(i);
     		System.out.println("Order value of " + i + ". object at off heap pool: " + obj.getOrder());
     	}
+    	
+		System.out.println("\n\n");
+	}
+	
+	private static void demoEagerReferencedObjectOffHeapPool(OffHeapService offHeapService) {
+		EagerReferencedObjectOffHeapPool<SampleClass> eagerReferencedSequentialObjectPool = 
+				offHeapService.createOffHeapPool(
+						new SequentialObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								type(SampleClass.class).
+								objectCount(ELEMENT_COUNT).
+								referenceType(ObjectPoolReferenceType.EAGER_REFERENCED).
+							build());
+							
+		System.out.println("Eager Referenced Sequential Off Heap Object Pool for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = eagerReferencedSequentialObjectPool.get();
+    		obj.setOrder(i);
+    		System.out.println("New object has been retrieved from off heap pool and set order value to " + i);
+    	}
+    	
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = eagerReferencedSequentialObjectPool.getAt(i);
+    		System.out.println("Order value of " + i + ". object at off heap pool: " + obj.getOrder());
+    	}
+    	
+		System.out.println("\n\n");
+	}
+
+	private static void demoComplexTypeArrayOffHeapPool(OffHeapService offHeapService) {
+		ComplexTypeArrayOffHeapPool<SampleClass> complexTypeArrayPoolWithNoInit = 
+				offHeapService.createOffHeapPool(
+						new ArrayOffHeapPoolCreateParameterBuilder<SampleClass>().
+								type(SampleClass.class).
+								length(ELEMENT_COUNT).
+								initializeElements(false).
+							build());
+		
+		System.out.println("Complex Type Off Heap Array Pool with no initialization for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		SampleClass[] complexArrayWithNoInit = complexTypeArrayPoolWithNoInit.getArray();
+		
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = new SampleClass();
+    		obj.setOrder(i);
+    		complexTypeArrayPoolWithNoInit.setAt(obj, i); // Note that "array[i] = obj" is not valid, because JVM doesn't know array created at off-heap
+    		System.out.println("New complex typed element has been created on heap and " + 
+    				"assigned to off heap pool array " + i);
+    	}
+   
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		SampleClass obj = complexArrayWithNoInit[i];
+    		System.out.println("Order value of " + i + ". element at off heap pool: " + obj.getOrder());
+    	}
+    	
+		System.out.println("\n\n");
+	}
+	
+	private static void demoPrimitiveTypeArrayOffHeapPool(OffHeapService offHeapService) {
+		PrimitiveTypeArrayOffHeapPool<Integer, int[]> primitiveTypeArrayPool = 
+				offHeapService.createOffHeapPool(
+						new ArrayOffHeapPoolCreateParameterBuilder<Integer>().
+								type(Integer.class).
+								length(ELEMENT_COUNT).
+								usePrimitiveTypes(true).
+							build());
+		
+		System.out.println("Primitive Type Off Heap Array Pool for class " + 
+				int.class.getName() + " has been allocated ...");
+		
+		int[] primitiveArray = primitiveTypeArrayPool.getArray();
+		
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		primitiveArray[i] = i;
+    		System.out.println("New pritimive typed element has been assigned to off heap pool array " + i);
+    	}
+    	
+    	for (int i = 0; i < ELEMENT_COUNT; i++) {
+    		int order = primitiveArray[i];
+    		System.out.println("Order value of " + i + ". element at off heap pool: " + order);
+    	}	
+    	
+    	System.out.println("\n\n");
+	}
+
+	private static void demoExtendableObjectOffHeapPoolWithLazyReferenceObjectOffHeapPool(OffHeapService offHeapService) {
+		LazyReferencedObjectOffHeapPool<SampleClass> sequentialObjectPool = 
+				offHeapService.createOffHeapPool(
+						new SequentialObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								type(SampleClass.class).
+								objectCount(ELEMENT_COUNT).
+								referenceType(ObjectPoolReferenceType.LAZY_REFERENCED).
+							build());
+
+		ExtendableObjectOffHeapPool<SampleClass> extendableObjectPool =
+				offHeapService.createOffHeapPool(
+						new ExtendableObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								forkableObjectOffHeapPool(sequentialObjectPool).
+							build());
+		
+		System.out.println("Extendable Lazy Referenced Sequential Off Heap Object Pool for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		List<SampleClass> objList = new ArrayList<SampleClass>();
+		
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = extendableObjectPool.get();
+    		obj.setOrder(i);
+    		objList.add(obj);
+    		System.out.println("New object has been retrieved from off heap pool and set order value to " + i);
+    	}
+    	
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = objList.get(i);
+    		System.out.println("Order value of " + i + ". object at off heap pool: " + obj.getOrder());
+    	}
+    	
+    	System.out.println("\n\n");
+	}
+	
+	private static void demoExtendableObjectOffHeapPoolWithEagerReferenceObjectOffHeapPool(OffHeapService offHeapService) {
+		EagerReferencedObjectOffHeapPool<SampleClass> sequentialObjectPool = 
+				offHeapService.createOffHeapPool(
+						new SequentialObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								type(SampleClass.class).
+								objectCount(ELEMENT_COUNT).
+								referenceType(ObjectPoolReferenceType.EAGER_REFERENCED).
+							build());
+   
+		ExtendableObjectOffHeapPool<SampleClass> extendableObjectPool =
+				offHeapService.createOffHeapPool(
+						new ExtendableObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								forkableObjectOffHeapPool(sequentialObjectPool).
+							build());
+		
+		System.out.println("Extendable Eager Referenced Sequential Off Heap Object Pool for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		List<SampleClass> objList = new ArrayList<SampleClass>();
+		
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = extendableObjectPool.get();
+    		obj.setOrder(i);
+    		objList.add(obj);
+    		System.out.println("New object has been retrieved from off heap pool and set order value to " + i);
+    	}
+    	
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = objList.get(i);
+    		System.out.println("Order value of " + i + ". object at off heap pool: " + obj.getOrder());
+    	}
+    	
+    	System.out.println("\n\n");
+	}
+	
+	private static void demoExtendableObjectOffHeapPoolWithDefaultObjectOffHeapPool(OffHeapService offHeapService) {
+		ExtendableObjectOffHeapPool<SampleClass> extendableObjectPool =
+				offHeapService.createOffHeapPool(
+						new DefaultExtendableObjectOffHeapPoolCreateParameterBuilder<SampleClass>().
+								elementType(SampleClass.class).
+							build());
+		
+		System.out.println("Extendable Default Off Heap Object Pool for class " + 
+				SampleClass.class.getName() + " has been allocated ...");
+		
+		List<SampleClass> objList = new ArrayList<SampleClass>();
+		
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = extendableObjectPool.get();
+    		obj.setOrder(i);
+    		objList.add(obj);
+    		System.out.println("New object has been retrieved from off heap pool and set order value to " + i);
+    	}
+    	
+    	for (int i = 0; i < TOTAL_ELEMENT_COUNT; i++) {
+    		SampleClass obj = objList.get(i);
+    		System.out.println("Order value of " + i + ". object at off heap pool: " + obj.getOrder());
+    	}
+    	
+    	System.out.println("\n\n");
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////
